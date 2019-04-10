7196f869ed90966347ac198063a68936c2a557cb
YouCii <550341130@qq.com>
2019-04-10 21:59:32 +0800

    1. init

---
+++ app/src/main/java/com/youcii/algorithm/structure/LinkedNode.java
@@ -0,0 +1,26 @@
+package com.youcii.algorithm.structure;
+
+public class LinkedNode {
+    public int val;
+    public LinkedNode next;
+
+    public LinkedNode(int x) {
+        val = x;
+    }
+
+    // 遍历链表返回拼接字符串
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder().append(val);
+        LinkedNode current = next;
+        while (current != null) {
+            builder.append("->").append(current.val);
+            current = current.next;
+            if (current == this) {
+                builder.append("->ring");
+                break;
+            }
+        }
+        return builder.toString();
+    }
+}
